05ea0d3fbbf2573bc6c50cea5252648388fee6ed
san1e <sklymenko1777@gmail.com>
2025-03-12 23:23:13 +0200

    add tracker

---
+++ app/src/main/java/com/example/tracker/LocationTrackerService.kt
@@ -0,0 +1,87 @@
+package com.example.tracker
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+class LocationTrackerService:Service() {
+
+    private val scope = CoroutineScope(
+        SupervisorJob() + Dispatchers.IO
+    )
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+
+    override fun onStartCommand(
+        intent: Intent?, flags: Int, startId: Int
+    ): Int {
+
+        when (intent?.action) {
+            Action.START.name -> start()
+            Action.STOP.name -> stop()
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    @SuppressLint("NotificationPermission")
+    private fun start() {
+
+        val locationManager = LocationManager(applicationContext)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification = NotificationCompat
+            .Builder(this, LOCATION_CHANNEL)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Location Tracker")
+            .setStyle(NotificationCompat.BigTextStyle())
+
+        startForeground(1, notification.build())
+
+        scope.launch {
+            locationManager.trackLocation().collect { location ->
+                val latitude = location.latitude.toString()
+                val longitude = location.longitude.toString()
+
+               notificationManager.notify(1,
+                   notification.setContentText(
+                       "Location: $latitude/ $longitude"
+                   ).build())
+
+            }
+        }
+
+    }
+
+    private fun stop() {
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+    }
+
+    enum class Action {
+        START, STOP
+    }
+
+    companion object {
+        const val LOCATION_CHANNEL = "location_channel"
+    }
+}
